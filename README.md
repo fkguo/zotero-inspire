def96c340484c1da7fb4728888fd670be1f3bd57
fkguo <fkguo@itp.ac.cn>
2022-05-25 16:41:00 +0800

    Version 0.1

---
+++ README.md
@@ -0,0 +1,51 @@
+# Zotero INSPIRE Metadata Updater
+
+This is an add-on for the excellent open-source reference manager [Zotero](https://github.com/zotero/zotero). It is useful as most papers from [arXiv](https://arxiv.org) will get published in journals later on. This add-on can be used to update the metadata of the selected item(s) in your Zotero library from the [INSPIRE](https://inspirehep.net) database. 
+
+
+
+## Usage
+
+- Right click a selected item or multiple selected items, click `Update INSPIRE Metadata`, then choose one of the two options: fetch the metadata with or without abstracts.
+
+- Right click a selected collection, then click `Update INSPIRE Metadata` `w/ Abstracts` or `w/o Abstracts`.
+
+- Automatically retrieve the metadata from INSPIRE when adding a new item to the Zotero library. Options with or without getting abstracts can be set through the `Tools` menu → `INSPIRE Metadata`…
+
+- The add-on will update the following fields:
+	- INSPIRE uses a unique `recid` for each publication in the database (called `control_number` in the `.json` file obtained via the [INSPIRE API](https://github.com/inspirehep/rest-api-doc)). The INSPIRE `recid` is set to the field of `Loc. in Archive` (and `INSPIRE` to `Archive`) for the selected Zotero item.
+		- This also enables us to write a look-up engine using this `recid` to exactly reach the INSPIRE page of that publication. The look-up engine can be added by editing the `engines.json` file in the `locate` folder of the Zotero Data Directory. The directory can be found by clicking `Zotero Preferences` → `Advanced` → `Files and Folders` → `Show Data Directory`. Add the following code to the `engines.file`, and put the path to the INSPIRE icon file after `"_icon":`:
+			```json
+			 {
+					"_name": "INSPIRE",
+					"_alias": "INSPIRE",
+					"_description": "INSPIRE",
+					"_icon": ,  // path to the INSPIRE icon,
+					"_hidden": false,
+					"_urlTemplate": "https://inspirehep.net/literature/{z:archiveLocation}",
+					"_urlNamespaces": {
+					  	"z": "http://www.zotero.org/namespaces/openSearch#"
+					}
+			}
+			```
+		
+	- `journal` (set to `Journal Abbr` in Zotero), `volume`, `year`, `pages` (either the page numbers or the modern article IDs), `issue`, and `abstract`.
+
+
+
+## Installation
+
+- Download the `.xpi` file of this add-on from https://github.com/fkguo/zotero-inspire/releases
+- In Zotero, the add-on can be installed by going to `Tools` → `Add-ons`, then click the top-right button and choose `Install Add-ons From File...`.
+
+## References
+
+I know basically nothing about javascript. This add-on was developed by modifying the codes of the following two add-ons:
+
+- https://github.com/bwiernik/zotero-shortdoi
+
+- https://github.com/eschnett/zotero-citationcounts
+
+## License
+
+Distributed under the Mozilla Public License (MPL) Version 2.0.
---
+++ chrome/content/scripts/zoteroinspire.js
@@ -0,0 +1,382 @@
+/* Zotero plugin for updating publication meta from the INSPIRE database
+
+FK Guo  | 2022-05-24
+
+The zotero part was written according to the following plugins:
+https://github.com/eschnett/zotero-citationcounts
+https://github.com/bwiernik/zotero-shortdoi
+*/
+
+if (typeof Zotero === 'undefined') {
+    Zotero = {};
+}
+Zotero.Inspire = {};
+
+
+/* Definitions of functions fetching and setting metadata using the INSPIRE api
+
+ getInspireMeta(item): get the INSPIRE recid and check for publication metadata for arxiv papers
+ setInspireMeta(item, metaInspire, operation): set the Zotero item fields
+
+ possible operations:
+ - full: fetching medatadata including abstract
+ - noabstract: no abstract
+ - none: nothing
+ */
+async function getInspireMeta(item) {
+
+    let meta = null;
+    let metaInspire = {};
+
+    let doi = item.getField('DOI');
+    let idtype = 'doi';
+    if (!doi) { // if no doi, 
+        idtype = 'arxiv';
+        let extra = item.getField('extra');
+        if (extra.includes('arXiv:')) {
+            const regexArxivId = 'arXiv:(.+)'
+            /* in this way, different situations are included:
+            New and old types of arXiv number; 
+            whether or not the arXiv line is at the end of extra
+            */
+            let arxiv_split = extra.match(regexArxivId)[1].split(' ')
+            arxiv_split[0] == '' ? doi = arxiv_split[1] : doi = arxiv_split[0]
+        } else {
+            const url = item.getField('url');
+            const patt = /(?:arxiv.org[/]abs[/]|arXiv:)([a-z.-]+[/]\d+|\d+[.]\d+)/i;
+            const m = patt.exec(url);
+            if (!m) {
+                return "No valid arxiv ID found in url"
+            } else {
+                doi = m[1]
+            }
+        }
+    }
+    if (!doi) {
+        return -1;
+    }
+    const edoi = encodeURIComponent(doi);
+
+    const url = "https://inspirehep.net/api/" + idtype + "/" + edoi;
+    let status = null;
+    const response = await fetch(url)
+        //   .then(response => response.json())
+        .then(response => {
+            if (response.status !== "404") {
+                status = 1;
+                return response.json()
+            }
+        })
+        .catch(err => null);
+
+    if (status == null) {
+        return -1;
+    }
+
+    try {
+        meta = response['metadata'];
+        metaInspire.recid = meta['control_number']
+        // get only the first doi
+        if (meta['dois'] !== undefined) {
+            metaInspire.DOI = meta['dois'][0].value
+        }
+        const publication_info = meta['publication_info']
+        if (publication_info) {
+            pubinfo_first = publication_info[0]
+            if (pubinfo_first.journal_title) {
+                let jAbbrev = ""
+                jAbbrev = pubinfo_first.journal_title;
+                metaInspire.journalAbbreviation = jAbbrev.replace(".", ". ");
+                metaInspire.volume = pubinfo_first.journal_volume;
+                if (pubinfo_first.artid) {
+                    metaInspire.pages = pubinfo_first.artid;
+                } else {
+                    metaInspire.pages = pubinfo_first.page_start
+                    pubinfo_first.page_end && (metaInspire.pages = metaInspire.pages + "-" + pubinfo_first.page_end)
+                }
+                metaInspire.date = pubinfo_first.year;
+                metaInspire.issue = pubinfo_first.journal_issue
+            }
+        }
+        let abstractInspire = meta['abstracts']
+        if (abstractInspire.length > 1) {
+            for (i = 0; i < abstractInspire.length; i++) {
+                abstractInspire[i].source == "arXiv" && (metaInspire.abstractNote = abstractInspire[i].value)
+            }
+        } else {
+            metaInspire.abstractNote = abstractInspire[0].value
+        }
+    } catch (err) {
+        return -1;
+    }
+
+    return metaInspire;
+}
+
+/*
+  set the metadata to Zotero items.
+  The zotero item fields are listed at 
+  https://www.zotero.org/support/dev/client_coding/javascript_api/search_fields
+*/
+function setInspireMeta(item, metaInspire, operation) {
+
+    if (item.getField('publicationTitle').includes('arXiv')) {
+        item.setField('publicationTitle', "")
+    }
+    if (item.getField('proceedingsTitle').includes('arXiv')) {
+        item.setField('proceedingsTitle', "")
+    }
+    // item.setField('archiveLocation', metaInspire);
+    if (metaInspire.recid !== -1 && metaInspire.recid !== undefined) {
+        item.setField('archive', "INSPIRE");
+        item.setField('archiveLocation', metaInspire.recid);
+
+        metaInspire.journalAbbreviation && item.setField('journalAbbreviation', metaInspire.journalAbbreviation);
+        // to avoid setting undefined to zotero items
+        metaInspire.volume && item.setField('volume', metaInspire.volume);
+        metaInspire.pages && item.setField('pages', metaInspire.pages);
+        metaInspire.date && item.setField('date', metaInspire.date);
+        metaInspire.issue && item.setField('issue', metaInspire.issue);
+        metaInspire.DOI && item.setField('DOI', metaInspire.DOI);
+        if (operation == "full" && metaInspire.abstractNote) {
+            item.setField('abstractNote', metaInspire.abstractNote)
+        }
+    }
+}
+
+// Preference managers
+
+Zotero.Inspire.getPref = function (pref) {
+    return Zotero.Prefs.get('extensions.inspire.' + pref, true)
+};
+
+Zotero.Inspire.setPref = function (pref, value) {
+    return Zotero.Prefs.set('extensions.inspire.' + pref, value, true)
+};
+
+// Startup - initialize plugin
+
+Zotero.Inspire.init = function () {
+    Zotero.Inspire.resetState("initial");
+
+    // Register the callback in Zotero as an item observer
+    const notifierID = Zotero.Notifier.registerObserver(
+        Zotero.Inspire.notifierCallback, ['item']);
+
+    // Unregister callback when the window closes (important to avoid a memory leak)
+    window.addEventListener('unload', function (e) {
+        Zotero.Notifier.unregisterObserver(notifierID);
+    }, false);
+};
+
+Zotero.Inspire.notifierCallback = {
+    notify: function (event, type, ids, extraData) {
+        if (event == 'add') {
+            switch (Zotero.Inspire.getPref("autoretrieve")) {
+                case "full":
+                    Zotero.Inspire.updateItems(Zotero.Items.get(ids), "full");
+                    break;
+                case "noabstract":
+                    Zotero.Inspire.updateItems(Zotero.Items.get(ids), "noabstract");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+};
+
+// Controls for Tools menu
+
+// *********** Set the checkbox checks, from pref
+Zotero.Inspire.setCheck = function () {
+    let tools_full = document.getElementById(
+        "menu_Tools-inspire-menu-popup-full");
+    let tools_noabstract = document.getElementById(
+        "menu_Tools-inspire-menu-popup-noabstract");
+    let tools_none = document.getElementById(
+        "menu_Tools-inspire-menu-popup-none");
+    const pref = Zotero.Inspire.getPref("autoretrieve");
+    tools_full.setAttribute("checked", Boolean(pref === "full"));
+    tools_noabstract.setAttribute("checked", Boolean(pref === "noabstract"));
+    tools_none.setAttribute("checked", Boolean(pref === "none"));
+};
+
+// *********** Change the checkbox, topref
+Zotero.Inspire.changePref = function changePref(option) {
+    Zotero.Inspire.setPref("autoretrieve", option);
+};
+
+/**
+ * Open the preference window
+ */
+Zotero.Inspire.openPreferenceWindow = function (paneID, action) {
+    const io = {
+        pane: paneID,
+        action: action
+    };
+    window.openDialog(
+        'chrome://zoteroinspire/content/options.xul',
+        'inspire-pref',
+        'chrome,titlebar,toolbar,centerscreen' + Zotero.Prefs.get('browser.preferences.instantApply', true) ? 'dialog=no' : 'modal',
+        io
+    );
+};
+
+
+Zotero.Inspire.resetState = function (operation) {
+    if (operation == "initial") {
+        if (Zotero.Inspire.progressWindow) {
+            Zotero.Inspire.progressWindow.close();
+        }
+        Zotero.Inspire.current = -1;
+        Zotero.Inspire.toUpdate = 0;
+        Zotero.Inspire.itemsToUpdate = null;
+        Zotero.Inspire.numberOfUpdatedItems = 0;
+        Zotero.Inspire.counter = 0;
+        error_norecid = null;
+        error_norecid_shown = false;
+        final_count_shown = false;
+        return;
+    } else {
+        if (error_norecid) {
+            Zotero.Inspire.progressWindow.close();
+            const icon = "chrome://zotero/skin/cross.png";
+            if (error_norecid && !error_norecid_shown) {
+                let progressWindowNoRecid = new Zotero.ProgressWindow({
+                    closeOnClick: true
+                });
+                progressWindowNoRecid.changeHeadline("INSPIRE recid not found");
+                if (Zotero.Inspire.getPref("tag_norecid") !== "") {
+                    progressWindowNoRecid.progress = new progressWindowNoRecid.ItemProgress(icon, "No INSPIRE recid was found for some items. These have been tagged with '" + Zotero.Inspire.getPref("tag_norecid") + "'.");
+                } else {
+                    progressWindowNoRecid.progress = new progressWindowNoRecid.ItemProgress(icon, "No INSPIRE recid was found for some items.");
+                }
+                progressWindowNoRecid.progress.setError();
+                progressWindowNoRecid.show();
+                progressWindowNoRecid.startCloseTimer(8000);
+                error_norecid_shown = true;
+            }
+        } else {
+            if (!final_count_shown) {
+                const icon = "chrome://zotero/skin/tick.png";
+                Zotero.Inspire.progressWindow = new Zotero.ProgressWindow({
+                    closeOnClick: true
+                });
+                Zotero.Inspire.progressWindow.changeHeadline("Finished");
+                Zotero.Inspire.progressWindow.progress = new Zotero.Inspire.progressWindow.ItemProgress(icon);
+                Zotero.Inspire.progressWindow.progress.setProgress(100);
+                if (operation == "full" || operation == "noabstract") {
+                    Zotero.Inspire.progressWindow.progress.setText(
+                        "INSPIRE metadata updated for " +
+                        Zotero.Inspire.counter + " items.");
+                }
+                Zotero.Inspire.progressWindow.show();
+                Zotero.Inspire.progressWindow.startCloseTimer(4000);
+                final_count_shown = true;
+            }
+        }
+        return;
+    }
+};
+
+Zotero.Inspire.updateSelectedCollection = (operation) => {
+    const collection = ZoteroPane.getSelectedCollection();
+    if (collection) {
+        const items = collection.getChildItems(false, false);
+        Zotero.Inspire.updateItems(items, operation);
+    }
+};
+
+Zotero.Inspire.updateSelectedItems = function (operation) {
+    Zotero.Inspire.updateItems(ZoteroPane.getSelectedItems(), operation);
+};
+
+Zotero.Inspire.updateItems = function (items0, operation) {
+    const items = items0.filter(item => !item.isFeedItem);
+
+    if (items.length === 0 ||
+        Zotero.Inspire.numberOfUpdatedItems <
+        Zotero.Inspire.toUpdate) {
+        return;
+    }
+
+    Zotero.Inspire.resetState("initial");
+    Zotero.Inspire.toUpdate = items.length;
+    Zotero.Inspire.itemsToUpdate = items;
+
+    // Progress Windows
+    Zotero.Inspire.progressWindow =
+        new Zotero.ProgressWindow({
+            closeOnClick: false
+        });
+    const icon = 'chrome://zotero/skin/toolbar-advanced-search' +
+        (Zotero.hiDPI ? "@2x" : "") + '.png';
+    if (operation == "full" || operation == "noabstract") {
+        Zotero.Inspire.progressWindow.changeHeadline(
+            "Getting INSPIRE metadata", icon);
+    }
+    const inspireIcon =
+        'chrome://zoteroinspire/skin/inspire' +
+        (Zotero.hiDPI ? "@2x" : "") + '.png';
+    Zotero.Inspire.progressWindow.progress =
+        new Zotero.Inspire.progressWindow.ItemProgress(
+            inspireIcon, "Retrieving INSPIRE metadata.");
+    Zotero.Inspire.updateNextItem(operation);
+};
+
+Zotero.Inspire.updateNextItem = function (operation) {
+    Zotero.Inspire.numberOfUpdatedItems++;
+
+    if (Zotero.Inspire.current == Zotero.Inspire.toUpdate - 1) {
+        Zotero.Inspire.progressWindow.close();
+        Zotero.Inspire.resetState(operation);
+        return;
+    }
+
+    Zotero.Inspire.current++;
+
+    // Progress Windows
+    const percent = Math.round((Zotero.Inspire.numberOfUpdatedItems / Zotero.Inspire.toUpdate) * 100);
+    Zotero.Inspire.progressWindow.progress.setProgress(percent);
+    Zotero.Inspire.progressWindow.progress.setText(
+        "Item " + Zotero.Inspire.current + " of " +
+        Zotero.Inspire.toUpdate);
+    Zotero.Inspire.progressWindow.show();
+
+    Zotero.Inspire.updateItem(
+        Zotero.Inspire.itemsToUpdate[Zotero.Inspire.current],
+        operation);
+};
+
+Zotero.Inspire.updateItem = async function (item, operation) {
+    if (operation == "full" || operation == "noabstract") {
+
+        const metaInspire = await getInspireMeta(item);
+        // if (metaInspire !== {}) {
+        if (metaInspire.recid !== -1 && metaInspire.recid !== undefined) {
+            if (item.hasTag(Zotero.Inspire.getPref("tag_norecid"))) {
+                item.removeTag(Zotero.Inspire.getPref("tag_norecid"));
+                item.saveTx();
+            }
+            setInspireMeta(item, metaInspire, operation);
+            item.saveTx();
+            Zotero.Inspire.counter++;
+        } else {
+            if (Zotero.Inspire.getPref("tag_norecid") !== "" && !item.hasTag(Zotero.Inspire.getPref("tag_norecid"))) {
+                item.addTag(Zotero.Inspire.getPref("tag_norecid"), 1);
+                item.saveTx();
+            }
+        }
+        Zotero.Inspire.updateNextItem(operation);
+
+    } else {
+        Zotero.Inspire.updateNextItem(operation);
+    }
+};
+
+if (typeof window !== 'undefined') {
+    window.addEventListener('load', function (e) {
+        Zotero.Inspire.init();
+    }, false);
+}
